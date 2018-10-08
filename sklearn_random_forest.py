c4993a941e569a0702e52b02093cec0e0f1594c0
Bharat Kunwar <b.kunwar@gmail.com>
2018-10-08 09:11:13 +0100

    Update sklearn_random_forest.py to reflect changes to random_forest.py
    Cross validation accuracy for 1 trees: 1.0
    Test accuracy for 1 trees: 0.6190476190476191
    Cross validation accuracy for 4 trees: 1.0
    Test accuracy for 4 trees: 1.0
    Cross validation accuracy for 16 trees: 1.0
    Test accuracy for 16 trees: 1.0
    ./sklearn_random_forest.py  1.66s user 0.45s system 57% cpu 3.667 total

--- sklearn_random_forest.py
+++ sklearn_random_forest.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
-import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from random_forest import CrossValidationSplitter
 
 if __name__ == "__main__":
     data = pd.read_csv('resources/sonar.all-data.csv', header=None).values
-    for n_tree in [1, 4, 16]:
-        accuracies = []
+    for n_trees in [1, 4, 16]:
+        cval_accs = []
+        test_accs = []
         model = None
         splitter = CrossValidationSplitter(data, k_fold=5, rate=0.9)
-        for train_data, validate_data in splitter:
+        for train_data, validate_data, test_data in splitter:
             model = RandomForestClassifier(
-                n_estimators=n_tree,
+                n_estimators=n_trees,
                 max_depth=5,
                 min_samples_leaf=1,
             )
             X,y = train_data[:,:-1], train_data[:,-1]
-            model.fit(X,y)
             X_val,y_val = validate_data[:,:-1], validate_data[:,-1]
-            accuracies.append(model.score(X,y))
-        validation_accuracy = np.mean(accuracies)
-        X,y = splitter.test_data[:,:-1], splitter.test_data[:,-1]
-        test_accuracy = model.score(X,y)
+            X_test,y_test = test_data[:,:-1], test_data[:,-1]
+            model.fit(X,y)
+            cval_accs.append(model.score(X_val,y_val))
+            test_accs.append(model.score(X_test,y_test))
+        cval_acc = sum(cval_accs)/k_fold
+        test_acc = sum(test_accs)/k_fold
         print(
-            f"Mean cross validation accuracy for {n_tree} trees: {validation_accuracy}")
-        print(f"Test accuracy for {n_tree} trees: {test_accuracy}")
+            f"Cross validation accuracy for {n_trees} trees: {cval_acc}")
+        print(f"Test accuracy for {n_trees} trees: {test_acc}")
